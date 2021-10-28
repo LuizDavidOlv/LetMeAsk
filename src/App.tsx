5919645b205d595cf691af07bdd61f76b25ebe1a
Luiz David <luizdavid.93@hotmail.com>
2021-10-28 07:36:38 -0300

    Tela de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import {Button} from './components/Button'
 import {ButtonChildren} from './components/ButtonChildren'
 import {ButtonCounter} from './components/ButtonCounter'
+import { Home } from "./pages/Home"
 
 function App() 
 {
   return (
-    <div>
-      <Button text="Button 1"/>
-      <ButtonChildren>Clique aqui!</ButtonChildren>
-      <ButtonCounter />
-    </div>
+    // <div>
+    //   <Button text="Button 1"/>
+    //   <ButtonChildren>Clique aqui!</ButtonChildren>
+    //   <ButtonCounter />
+    // </div>
+    <Home/>
   );
 }
 
