5dabe4c0635fc3769229215d9be995b48ca0a63c
Luiz David <luizdavid.93@hotmail.com>
2021-11-09 06:37:00 -0300

    Criação de Sala

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 
 export const TestContext = createContext("");
@@ -15,7 +16,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
-
+          <Route path="/rooms/:id"component={Room} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
